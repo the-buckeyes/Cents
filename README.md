b568469b77964f27c1bc7206c755b8024b828a17
Nathan A Sculli <nathan@vegasbuckeye.com>
2020-03-11 06:59:05 -0700

    Initial work

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # cents
 
 A small crate for handling monetary values as in integer number of cents.
+
+### Example Usage
+
+```rust
+use cents::Cents;
+
+// Create a new cents object directly.
+let money = Cents::new(21, 21);
+
+// compare it against a cents object created from a u64 value.
+assert_eq!(Cents::new(42, 42), 4242.into());
+
+
+// add it to an u64 value.
+assert_eq!(money + 2121, Cents::new(42, 42);
+
+// Add two cents
+assert_eq!(Cents::from(2121) + Cents::from(2121), Cents::new(42, 42));
+```
+
+### Features
+
+- Addition (Self, u64)
+- Subtraction (Self)
+- Multiplication (Self, u64)
+- Conversion from BigInt
+- Conversion from u64
+- Equality (Self)
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,158 @@
+use num::{BigInt, ToPrimitive};
+use std::{
+    cmp::Ordering,
+    convert::TryInto,
+    ops::{Add, AddAssign, Mul, Sub},
+};
+
+#[derive(Clone, Copy, Debug)]
+pub struct Cents {
+    value: u64,
+}
+
+impl Cents {
+    pub fn new(dollars: i64, cents: u8) -> Self {
+        let dollars: u64 = num::abs(dollars).try_into().unwrap();
+
+        Self {
+            value: (dollars * 100) + cents as u64,
+        }
+    }
+}
+
+impl From<Cents> for u64 {
+    fn from(cents: Cents) -> u64 {
+        cents.value
+    }
+}
+
+impl From<u64> for Cents {
+    fn from(int: u64) -> Self {
+        Self { value: int }
+    }
+}
+
+impl From<BigInt> for Cents {
+    fn from(int: BigInt) -> Self {
+        Self {
+            value: num::abs(int).to_u64().unwrap(),
+        }
+    }
+}
+
+impl From<&BigInt> for Cents {
+    fn from(int: &BigInt) -> Self {
+        Self {
+            value: num::abs(int.to_owned()).to_u64().unwrap(),
+        }
+    }
+}
+
+impl Add for Cents {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            value: self.value + other.value,
+        }
+    }
+}
+
+impl Add<u64> for Cents {
+    type Output = Self;
+
+    fn add(self, other: u64) -> Self {
+        Self {
+            value: self.value + other,
+        }
+    }
+}
+
+impl AddAssign for Cents {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            value: self.value + other.value,
+        }
+    }
+}
+
+impl AddAssign<u64> for Cents {
+    fn add_assign(&mut self, other: u64) {
+        *self = Self {
+            value: self.value + other,
+        }
+    }
+}
+
+impl Mul for Cents {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self {
+            value: self.value + rhs.value,
+        }
+    }
+}
+
+impl Mul<u64> for Cents {
+    type Output = Self;
+
+    fn mul(self, rhs: u64) -> Self::Output {
+        Self {
+            value: self.value * rhs,
+        }
+    }
+}
+
+impl PartialEq for Cents {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl Eq for Cents {}
+
+impl PartialOrd for Cents {
+    fn partial_cmp(&self, other: &Cents) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Cents {
+    fn cmp(&self, other: &Cents) -> Ordering {
+        self.value.cmp(&other.value)
+    }
+}
+
+impl Sub for Cents {
+    type Output = Cents;
+
+    fn sub(self, other: Cents) -> Cents {
+        Self {
+            value: self.value - other.value,
+        }
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod test {
+    use super::Cents;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_add_to_u64() {
+        let actual = Cents::new(21, 21) + 2121;
+        let expected = Cents::new(42, 42);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_compare_to_u64() {
+        assert_eq!(Cents::new(42, 42), 4242.into());
+    }
+
+    #[test]
+    fn test_add_two_cents() {
+        assert_eq!(Cents::from(2121) + Cents::from(2121), Cents::new(42, 42));
     }
 }
